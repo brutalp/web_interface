17b5ccc18e5c21b049f13159ac25042e5363fad5
brutalp <brutal616@gmail.com>
2020-03-05 14:47:45 +0300

    add second DB

--- web_interface/web_interface/settings.py
+++ web_interface/web_interface/settings.py
@@ -79,7 +79,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    'ASX': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ARX',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
 }
 
 
