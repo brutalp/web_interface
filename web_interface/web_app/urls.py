06847d8b9229a2d9ff0ce8c550f294a9001ffadc
brutalp <brutal616@gmail.com>
2020-03-11 15:30:31 +0300

    replace table for show

--- web_interface/web_app/urls.py
+++ web_interface/web_app/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import ListView
-from web_app.models import Object
+from web_app.models import Measurement
 from . import views
 
 urlpatterns = [
     # url(r'^$', views.web_app, name='web'),
     # url(r'^$', views.ObjectView.as_view(), name='object'),
-    url(r'^$', ListView.as_view(queryset=Object.objects.all(), template_name='web_app/web.html')),
+    url(r'^$', ListView.as_view(queryset=Measurement.objects.all(), template_name='web_app/web.html')),
 ]
