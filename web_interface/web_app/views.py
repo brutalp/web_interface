60be29b9fa79b603d943365dbca779cacc8bcee8
brutalp <brutal616@gmail.com>
2020-03-12 14:37:52 +0300

    add pagination into template

--- web_interface/web_app/views.py
+++ web_interface/web_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
 from web_app.models import *
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+def PagView(request):
+    valueList = Value.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(valueList, 50)
+    try:
+        values = paginator.page(page)
+    except PageNotAnInteger:
+        values = paginator.page(1)
+    except EmptyPage:
+        values = paginator.page(paginator.num_pages)
+    return render(request, 'web_app/web.html', {'values': values})
 
 
 # def web_app(request):
