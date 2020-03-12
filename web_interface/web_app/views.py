a00cc614711151d673339fad2897bb7fce388df1
brutalp <brutal616@gmail.com>
2020-03-12 12:28:24 +0300

    add Paginator

--- web_interface/web_app/views.py
+++ web_interface/web_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
-from web_app.models import Object
+from web_app.models import *
+from django.core.paginator import Paginator
 
 
 # def web_app(request):
