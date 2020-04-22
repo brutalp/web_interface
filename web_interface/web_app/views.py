e27007d316d23c69a5c9575e8289e32afe12c26d
brutalp <brutal616@gmail.com>
2020-04-22 10:36:51 +0300

    fix pagination

--- web_interface/web_app/views.py
+++ web_interface/web_app/views.py
@@ -1,29 +1,63 @@
-from django.shortcuts import render
 from django.views import generic
-from web_app.models import *
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import *
+from django.views.generic import View
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 
 
-def PagView(request):
-    valueList = Measurement.objects.all().order_by('id')
-    if request.method == 'POST':
-        pass
-        print('POST')
-        print(request)
-    else:
-        print('GET')
+class PagView(View):
+
+    def get(self, request, page_id=1):
+        valueList = Measurement.objects.all().order_by('id')
         page = request.GET.get('page', 1)
-        # value_on_page = request.POST.get(value)
         paginator = Paginator(valueList, 50)
-        # paginator = Paginator(valueList, value)
-        # paginator = Paginator(valueList, 50)
         try:
-            values = paginator.page(page)
-        except PageNotAnInteger:
-            values = paginator.page(1)
-        except EmptyPage:
-            values = paginator.page(paginator.num_pages)
+            values = paginator.page(page_id)
+            values.num_pages_tuple = tuple(range(paginator.num_pages))
+        except:
+            return redirect(reverse('pagination'))
         return render(request, 'web_app/web.html', {'values': values})
+
+
+# def PagView(request):
+#     valueList = Measurement.objects.all().order_by('id')
+#     if request.method == 'POST':
+#         pass
+#         print('POST')
+#         html = '<html><body>'
+#         x = []
+#         for key, value in request.POST.items():
+#             html += f'{key}: {value}<br>'
+#             x = x.append(value)
+#         html += '</html></body>'
+#         print(x)
+#         return HttpResponse(html)
+#     else:
+#         print('GET')
+#         page = request.GET.get('page', 1)
+#         # value_on_page = request.POST.get(value)
+#         # paginator = Paginator(valueList, 50)
+#         # paginator = Paginator(valueList, value)
+#         # paginator = Paginator(valueList, 50)
+#
+#         paginator = Paginator(valueList, 50)
+#         try:
+#             values = paginator.page(page)
+#             values.num_pages_tuple = tuple(range(paginator.num_pages))
+#         except:
+#             return redirect(reverse('pagination'))
+#         # return render(request, 'wine_app/shop.html', {'wines': wines})
+#
+#         # try:
+#         #     values = paginator.page(page)
+#         # except PageNotAnInteger:
+#         #     values = paginator.page(1)
+#         # except EmptyPage:
+#         #     values = paginator.page(paginator.num_pages)
+#         return render(request, 'web_app/web.html', {'values': values})
+
 
 def PagViewTest(request):
     valueList = Measurement.objects.all().order_by('id')
--- web_interface/web_interface/urls.py
+++ web_interface/web_interface/urls.py
@@ -18,5 +18,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url('web/', include('web_app.urls')),
+    url('', include('web_app.urls')),
 ]
